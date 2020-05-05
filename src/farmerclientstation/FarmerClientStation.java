1052bf0ac565f543d9114007a77ede073e4f44a3
arek <49416568+amich1@users.noreply.github.com>
2020-05-05 12:12:37 +0100

    update

--- src/farmerclientstation/FarmerClientStation.java
+++ src/farmerclientstation/FarmerClientStation.java
@@ -17,117 +17,108 @@ import javax.swing.*;
 import org.json.simple.*;
 
 class ServerConnection {
+  int id;
+  Socket socket;
+  String host;
+  int port;
+  PrintWriter socketWriter;
+  BufferedReader socketReader;
+  boolean init = false;
+  SwingWorker<Void, Void> loop;
+  public boolean authed = false;
 
-    Socket socket;
-    String host;
-    int port;
-    PrintWriter socketWriter;
-    BufferedReader socketReader;
-    boolean init = false;
-    SwingWorker<Void, Void> loop;
-    public boolean authed = false;
+  ServerConnection(String host, int port) {
+    this.host = host;
+    this.port = port;
+    Random rand = new Random();
+    id = rand.nextInt(999999);
+  }
 
-    ServerConnection(String host, int port) {
-        this.host = host;
-        this.port = port;
+  boolean init() {
+    if (init) {
+      return false;
     }
-
-    boolean init() {
-        if (init) {
-            return false;
-        }
-        try {
-            socket = new Socket(host, port);
-            socketWriter = new PrintWriter(socket.getOutputStream(), true);
-            socketReader = new BufferedReader(new InputStreamReader(socket.getInputStream()));
-        } catch (UnknownHostException ex) {
-            System.out.println("Server not found: " + ex.getMessage());
-        } catch (IOException ex) {
-            System.out.println("I/O error: " + ex.getMessage());
-        }
-        init = true;
-        return true;
+    try {
+      socket = new Socket(host, port);
+      socketWriter = new PrintWriter(socket.getOutputStream(), true);
+      socketReader = new BufferedReader(new InputStreamReader(socket.getInputStream()));
+    } catch (UnknownHostException ex) {
+      System.out.println("Server not found: " + ex.getMessage());
+    } catch (IOException ex) {
+      System.out.println("I/O error: " + ex.getMessage());
     }
+    init = true;
+    return true;
+  }
 
-    boolean auth(String user, String pass) {
-        JSONObject toSend = new JSONObject();
-        toSend.put("type", "station");
-        toSend.put("purpose", "auth");
-        toSend.put("username", user);
-        toSend.put("password", pass);
-        socketWriter.println(toSend);
-        try {
-            String line = socketReader.readLine();
-            JSONObject obj = (JSONObject) JSONValue.parse(line);
-            System.out.println(line);
-            if (obj.get("purpose").equals("auth")) {
-                if (obj.get("result").equals("pass")) {
-                    startLoop();
-                    authed = true;
-                    return true;
-                } else {
-                    return false;
-                }
-            }
-        } catch (IOException ex) {
-            
+  boolean auth() {
+    JSONObject toSend = new JSONObject();
+    toSend.put("clientType", "station");
+    toSend.put("purpose", "auth");
+    socketWriter.println(toSend.toJSONString());
+    try {
+      String line = socketReader.readLine();
+      JSONObject obj = (JSONObject) JSONValue.parse(line);
+      System.out.println(line);
+      if (obj.get("purpose").equals("auth")) {
+        if (obj.get("result").equals("pass")) {
+          startLoop();
+          authed = true;
+          return true;
         }
         return false;
-    }
 
-    void startLoop() {
-        loop = new SwingWorker<Void, Void>() {
-            @Override
-            protected Void doInBackground() throws Exception {
-                while (init) {
-                    try {
-                        String line = socketReader.readLine();
-                        //JSONObject obj = (JSONObject) JSONValue.parse(line);
-                        System.out.println(line);
-                    } catch (IOException ex) {
-                        System.out.println("I/O error occured");
-                    }
-                }
-                return null;
-            }
-        };
-        loop.execute();
+      }
+    } catch (IOException ex) {
+
     }
-    
-    void sendData(String data) {
-        JSONObject toSend = new JSONObject();
-        toSend.put("message", data);
-        socketWriter.println(toSend);
+    return false;
+  }
+
+  void sendData(JSONObject data) {
+    JSONObject toSend = new JSONObject();
+    toSend.put("clientType", "station");
+    toSend.put("purpose", "data");
+
+    toSend.put("data", data);
+    socketWriter.println(toSend);
+  }
+
+  void startLoop() {
+    while (init) {
+      try {
+        Thread.sleep(1000);
+      } catch (InterruptedException ie) {
+
+      }
+
+      JSONObject toSend = new JSONObject();
+      Random rand = new Random();
+      toSend.put("id", id);
+      toSend.put("temp", rand.nextInt(40));
+      toSend.put("humidity", rand.nextInt(1000));
+      toSend.put("wind", rand.nextInt(50));
+      toSend.put("lightLevel", rand.nextInt(100));
+      sendData(toSend);
     }
+  }
 }
-class SayHello extends TimerTask {
-    ServerConnection c;
-    String to;
-    SayHello(ServerConnection c, String to){
-        this.c = c;
-        this.to = to;
-    }
-    public void run() {
-        c.sendData(to);
-    }
-}
+
 public class FarmerClientStation {
-    static ServerConnection s;
-    static java.util.Timer t;
-    /**
-     * @param args the command line arguments
-     */
-    public static void main(String[] args) {
-        // TODO code application logic here
-        s = new ServerConnection("localhost", 1111);
-        s.init();
-        if(s.auth("stationloign", "stationpass")){
-            System.out.println("authed");
-        }
-        s.startLoop();
-        s.sendData("hello from station");
-        t = new java.util.Timer();
-        t.schedule(new SayHello(s, "Hello from server"), 0, 5000);
+
+  static ServerConnection s;
+  static java.util.Timer t;
+
+  /**
+   * @param args the command line arguments
+   */
+  public static void main(String[] args) {
+    // TODO code application logic here
+    s = new ServerConnection("localhost", 1111);
+    s.init();
+    if (s.auth()) {
+      System.out.println("authed");
     }
-    
+    s.startLoop();
+  }
 }
